69c2f1c7e51c5b66949ed0d308f06a23c8ee07c3
codylovell <codylovell94@gmail.com>
2018-06-01 14:53:57 -0400

    trying to figure out user login

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from app import app
+from app.views import index
+from flask import render_template, url_for, redirect, g, request
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = "Invalid credintials, Please try again."
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
 	app.run(debug = True)
